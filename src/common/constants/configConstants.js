72160b0b5c4acefeafe9d168b3a08c8745b96eaa
Hirendudhat009 <dudhat4744@gmail.com>
2025-03-10 13:01:32 +0530

    Feat: chanegs

--- src/common/constants/configConstants.js
+++ src/common/constants/configConstants.js
@@ -1,11 +1,20 @@
 module.exports = {
   baseUrl(path = null) {
+    if (process.env.USE_SERVER_MANAGED_SSL === "true") {
+      let url = `${process.env.BASE_URL}/api/v1`;
+      return url + (path ? `/${path}` : "");
+    }
+
     let url = `http://${process.env.HOST}:${process.env.PORT}`;
 
     return url + (path ? `/${path}` : "");
   },
 
   apiBaseUrl(path = null) {
+    console.log(
+      process.env.USE_SERVER_MANAGED_SSL,
+      process.env.USE_SERVER_MANAGED_SSL === "true"
+    );
     if (process.env.USE_SERVER_MANAGED_SSL === "true") {
       let url = `${process.env.BASE_URL}/api/v1`;
       return url + (path ? `/${path}` : "");
